3f54aff27b6dbf28726a4a96d0d2a10f0f09cd78
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-04-06 10:00:18 +0800

    feat(Theme): added custom font

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      "Sora",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+  },
+});
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Dashboard></Dashboard>
-    </>
+    </ThemeProvider>
   );
 }
 
